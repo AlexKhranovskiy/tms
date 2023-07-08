68dacf563b9664666cbbcf729ccbd13223434516
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2023-07-08 18:58:21 +0300

    Done getting all tasks

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+   public function index()
+   {
+       return response()->json(['data' => Task::all()]);
+   }
+
+   public function create(Request $request)
+   {
+
+   }
 }
